e4b85c4e7d88cd583c7f84abdcb99ce5fe192a79
ashishu007 <panditu2015@gmail.com>
2022-05-15 15:42:47 +0100

    eval res new

--- run.sh
+++ run.sh
@@ -6,23 +6,23 @@ do
     echo "=========================================================="
     echo "Season: $ses"
 
-    # echo "Creating Imp Player Classifier Data..."
-    # python3 utils/imp_players_clf_data.py -season $ses
+    echo "Creating Imp Player Classifier Data..."
+    python3 utils/imp_players_clf_data.py -season $ses
 
-    # echo "Creating Imp Player Classifier..."
-    # python3 player_clf/main.py -season $ses
+    echo "Creating Imp Player Classifier..."
+    python3 player_clf/main.py -season $ses
 
-    # echo "Creating Player Popularity Data..."
-    # python3 utils/calc_popularity.py -season $ses
+    echo "Creating Player Popularity Data..."
+    python3 utils/calc_popularity.py -season $ses
 
-    # echo "Extracting Concepts from Gold Summaries..."
-    # python3 utils/extract_from_gold.py -season $ses
+    echo "Extracting Concepts from Gold Summaries..."
+    python3 utils/extract_from_gold.py -season $ses
 
-    # echo "Creating Case-Base for ${ses} season..."
-    # python3 create_cb.py -side both -season $ses -pop
+    echo "Creating Case-Base for ${ses} season..."
+    python3 create_cb.py -side both -season $ses -pop
 
-    # echo "Generating Concepts on Test Set..."
-    # python3 gen_concepts.py -pop -season $ses
+    echo "Generating Concepts on Test Set..."
+    python3 gen_concepts.py -pop -season $ses
 
     echo "Evaluating..."
     python3 utils/non_rg.py len $ses
